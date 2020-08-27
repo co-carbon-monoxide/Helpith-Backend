c10fc8541abbc58d4779085392c5b85ece219246
Momeemt <momiimt@gmail.com>
2020-08-27 22:22:52 +0900

    👍 Add: 月日・家族からリストを呼ぶrouteを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :families
       resources :users
+      get 'lists/:family_id/:date' => "lists#show_by_date"
       resources :lists
       resources :house_works
       resources :tasks
