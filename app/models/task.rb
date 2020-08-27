f075379ca8948de0d8a49696a242f26e631ee37f
Momeemt <momiimt@gmail.com>
2020-08-27 22:22:34 +0900

    👍 Change: 実装前のmodelの関連付けを一時削除

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  belongs_to :user
-  belongs_to :house_work
+  # belongs_to :user
+  # belongs_to :house_work
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :tasks, dependent: :destroy
+  # has_many :tasks, dependent: :destroy
   belongs_to :family
 end
